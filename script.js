b8bd2e5d50998ffcbac4a1b85624be0f56190c93
Fabian Puchta <fabianpuchta@hotmail.de>
2025-07-13 14:10:38 +0200

    Fotogram first structure

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//Background change Function
+
+
+function change(id) {
+    document.getElementById('c').classList.remove('height')
+    document.getElementById('c').classList.add('backgroundImg');
+    let img = document.getElementById(id).src;
+    document.getElementById('c').style.backgroundImage = "url(" + img + ")";
+    document.getElementById('c').style.backgroundColor = "rgba(255,255,255,0.6)";
+    document.getElementById('c').style.backgroundBlendMode = "lighten";
+
+
+
+
+}
+
+function changeback() {
+    document.getElementById('c').classList.remove('backgroundImg');
+    document.getElementById('c').classList.add('height');
+    document.getElementById('c').style.backgroundImage = "initial";
+    document.getElementById('c').style.backgroundColor = "rgb(196 234 250)";
+    document.getElementById('c').style.backgroundBlendMode = "initial";
+
+}
+
+function dialog() {
+    document.getElementById("dialogWindow").open = true;
+}
+
+
