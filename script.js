76f14eff345ade6123b84d49d1e858f8de408e94
Fabian Puchta <fabianpuchta@hotmail.de>
2025-07-15 18:57:29 +0200

    work in progress dialog window

--- script.js
+++ script.js
@@ -33,3 +33,36 @@ function dialog_close() {
     element.open = false;
 }
 
+const imgArray = [
+    img = document.getElementById('pic1').src,
+    img2 = document.getElementById('pic2').src,
+    img3 = document.getElementById('pic3').src,
+    img4 = document.getElementById('pic4').src,
+    img5 = document.getElementById('pic5').src,
+    img6 = document.getElementById('pic6').src,
+    img7 = document.getElementById('pic7').src,
+    img8 = document.getElementById('pic8').src,
+    img9 = document.getElementById('pic9').src,
+    img10 = document.getElementById('pic10').src,
+    img11 = document.getElementById('pic11').src,
+    img12 = document.getElementById('pic12').src,
+]
+console.log(imgArray);
+
+
+
+
+
+
+
+
+function dialogHead(id) {
+    let head = document.getElementById(id).alt;
+    document.getElementById('dialogHeader').innerHTML = head;
+    let img = document.getElementById(id).src;
+    document.getElementById('dialogImg').src = img;
+
+    document.getElementById('dialogPosition').innerHTML = head;
+
+}
+
